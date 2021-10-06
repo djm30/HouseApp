3c1b8b9fa4521c01d943b79f2b2e36beb46343c8
Philip Cox <philipcox197@gmail.com>
2021-10-06 15:46:43 +0100

    Added some routes for task + rota

--- src/database/mongo_service.py
+++ src/database/mongo_service.py
@@ -14,3 +14,15 @@ class MongoService:
     def insert_into_collection(self, collection_name, db, data):
         mongo_collection = self.mongo_connection[db][collection_name]
         mongo_collection.insert_one(data)
+
+    def update_into_collection(self, collection_name, db, query, field, value):        
+        mongo_collection = self.mongo_connection[db][collection_name]
+        mongo_collection.find_one_and_update(
+            {
+                "_id" : mongo_collection.find(query).get("_id")
+            },
+            {
+                "$set": {field: value}
+            },
+            upsert=True
+        )
